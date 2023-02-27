35dbd19e1566f4e1ee8e14cc2f4c5c244231d1e3
hunter-meloche <123516285+hunter-meloche@users.noreply.github.com>
2023-02-27 16:44:58 -0500

    Update 217. Contains Duplicate.py

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -2,7 +2,7 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         
         # Sets in math cannot have duplicate values
-        # Converting the list to a set removes duplicate values
+        # Converting the list to a set removes duplicate values for O(n) time complexity
         # My previous O(n^2) solution used nested for loops to iterate and compare values
         # Credit to https://leetcode.com/problems/contains-duplicate/solutions/1215124/python-solutions/
         return len(set(nums)) < len(nums)
