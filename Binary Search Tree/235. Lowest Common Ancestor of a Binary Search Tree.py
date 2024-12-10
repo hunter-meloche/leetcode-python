eebe0e694219a3e1e97364a61b37f51fd54b4fa3
Hunter Meloche <123516285+hunter-meloche@users.noreply.github.com>
2024-12-09 21:27:59 -0500

    Create 235. Lowest Common Ancestor of a Binary Search Tree.py

---
+++ Binary Search Tree/235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -0,0 +1,18 @@
+# Another one I looked up because I didn't understand that the binary search tree will always be sorted with the lower being to the left and the higher on the right
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while True:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
